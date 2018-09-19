d9b2a36029151dd698f088027839543bf039af7d
esanchezlMBT <esanchezl@mainbit.com.mx>
2018-09-19 13:08:43 -0500

    Registro
    - Reiniciar dialogo

--- existe.js
+++ existe.js
@@ -2,14 +2,15 @@ var builder = require('botbuilder');
 
 // Opciones: Si  / No, registrar cuenta.
 var Registro = {
-    Si: 'Si',
+    Si: 'Si, estoy registrado',
     No: 'No, registrar cuenta',
+    Volver: 'Volver al inicio'
 };
 
 module.exports = [
 function (session) {
     session.send( 'Para este proceso debes tener registrada tu cuenta en el sistema de Doble Autenticación.');
-    builder.Prompts.choice(session, `¿Tu cuenta está registrada?`, [Registro.Si, Registro.No], { listStyle: builder.ListStyle.button });
+    builder.Prompts.choice(session, `¿Tu cuenta está registrada?`, [Registro.Si, Registro.No, Registro.Volver], { listStyle: builder.ListStyle.button });
 },
 function (session, result) {
     var selection = result.response.entity;
@@ -18,6 +19,8 @@ function (session, result) {
                 return session.beginDialog('reseteo');
             case Registro.No:
                 return session.beginDialog('registro');
+            case Registro.Volver:
+                return session.beginDialog('/');
         }
 }
 ]
